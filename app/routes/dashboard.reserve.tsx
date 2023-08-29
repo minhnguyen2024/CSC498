8e2e7f9a0e72117f5febacd2d0c47215e181f97f
Minh T. Nguyen <minhnguyen_2024@depauw.edu>
2023-08-29 10:07:40 -0400

    TODO:refactoring data partitioning to loader

--- app/routes/dashboard.reserve.tsx
+++ app/routes/dashboard.reserve.tsx
@@ -18,6 +18,10 @@ export async function loader({ request }: LoaderArgs) {
   const blocks: any = await getAllBlocks();
   const partitionedBy10 = partitionArrayByChunk(blocks, 10);
   const partitionedBy10By7 = partitionArrayByChunk(partitionedBy10, 7)
+  partitionedBy10By7.map((item: any) => {
+    console.log(item)
+    console.log("--------------------------")
+})
   return { partitionedBy10, partitionedBy10By7 };
 }
 
@@ -34,7 +38,66 @@ export async function action({ request }: ActionArgs) {
 export default function DashboardReserve() {
 //   let data: Array<object[]> = useLoaderData<typeof loader>();
   const { partitionedBy10, partitionedBy10By7 } = useLoaderData<typeof loader>();
-  console.log(partitionedBy10By7)
+//   console.log(partitionedBy10By7) //3D array
+
+    const renderCardsInColumn = (roomsByBlock: any[]) =>{
+        const cardItems = []
+        for (let i = 0; i < roomsByBlock.length; i++){
+            cardItems.push(<div key={i}>
+                <Card
+                  className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+                >
+                  <CardContent>
+                    <form method="post">
+                      {/* <input type="hidden" value={[entry]} name="_action" /> */}
+                      <Button className="bg-red-500 rounded text-white">
+                        Reserve
+                      </Button>
+                    </form>
+                  </CardContent>
+                </Card>
+              </div>)
+        }
+        return cardItems
+    //     return <div key={index}>
+    //     <Card
+    //       key={index}
+    //       className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+    //     >
+    //       <CardContent>
+    //         {numAvailableRooms}
+    //         <form method="post">
+    //           <input type="hidden" value={[entry]} name="_action" />
+    //           <Button className="bg-red-500 rounded text-white">
+    //             Reserve
+    //           </Button>
+    //         </form>
+    //       </CardContent>
+    //     </Card>
+    //   </div>
+    }
+
+    // const renderSingleCardItem = (roomsByBlock: any[], ) =>{
+    //     return <div key={index}>
+    //                 <Card
+    //                   key={index}
+    //                   className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+    //                 >
+    //                   <CardContent>
+    //                     {numAvailableRooms}
+    //                     <form method="post">
+    //                       <input type="hidden" value={[entry]} name="_action" />
+    //                       <Button className="bg-red-500 rounded text-white">
+    //                         Reserve
+    //                       </Button>
+    //                     </form>
+    //                   </CardContent>
+    //                 </Card>
+    //               </div>
+        
+    // }
+
+    
   return (
     <div>
       <p>Reserve</p>
@@ -45,7 +108,8 @@ export default function DashboardReserve() {
       <div className="flex h-full bg-white">
         {partitionedBy10By7.map((item: any, index: any) => {
           //this is one column of 7 cards, each card contains array of 10 rooms
-          //[[Array(10), Array(10),....]] => length = 7?
+          //[[Array(10), Array(10),....]] => length = 7
+          
           return (
             <div className="h-full w-40 border-r bg-gray-50" key={index}>
               {item.map((entry: any, index: any) => {
@@ -60,7 +124,6 @@ export default function DashboardReserve() {
                     : undefined,
                 );
                 const entryArr = [entry]
-                // console.log(item)
                 // console.log(`entryArr ${entryArr[0][9].id}`)
                 return (
                   <div key={index}>
@@ -84,6 +147,15 @@ export default function DashboardReserve() {
             </div>
           );
         })}
+        <div className="h-full w-40 border-r bg-gray-50" key={0}>
+            {renderCardsInColumn(partitionedBy10By7[0])}
+        </div>
+        <div className="h-full w-40 border-r bg-gray-50" key={1}></div>
+        <div className="h-full w-40 border-r bg-gray-50" key={2}></div>
+        <div className="h-full w-40 border-r bg-gray-50" key={3}></div>
+        <div className="h-full w-40 border-r bg-gray-50" key={4}></div>
+        <div className="h-full w-40 border-r bg-gray-50" key={5}></div>
+        <div className="h-full w-40 border-r bg-gray-50" key={6}></div>
         <div className="flex-1 p-6">
           <Outlet />
         </div>
