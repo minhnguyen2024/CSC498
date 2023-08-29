d3f175d07ce70974c98777fa9f991e76f45ea76a
Minh T. Nguyen <minhnguyen_2024@depauw.edu>
2023-08-29 16:34:53 -0400

    refactored to manage partitioning in loader

--- app/routes/dashboard.reserve.tsx
+++ app/routes/dashboard.reserve.tsx
@@ -17,145 +17,83 @@ export async function loader({ request }: LoaderArgs) {
   const userId = requireUserId(request);
   const blocks: any = await getAllBlocks();
   const partitionedBy10 = partitionArrayByChunk(blocks, 10);
-  const partitionedBy10By7 = partitionArrayByChunk(partitionedBy10, 7)
-  partitionedBy10By7.map((item: any) => {
-    console.log(item)
-    console.log("--------------------------")
-})
+  const partitionedBy10By7 = partitionArrayByChunk(partitionedBy10, 7);
   return { partitionedBy10, partitionedBy10By7 };
 }
 
 export async function action({ request }: ActionArgs) {
   const userId = requireUserId(request);
-  const formData = await request.formData();
-  let { _action } = Object.fromEntries(formData);
-  if (_action == "reserve") {
-    console.log("hello from action");
-  }
-  console.log(_action)
+  const body = await request.formData();
+  const items: string = body.get("items")?.toString() || ""
+    const blockIDByTime = JSON.parse(items)
   return null;
 }
 export default function DashboardReserve() {
-//   let data: Array<object[]> = useLoaderData<typeof loader>();
-  const { partitionedBy10, partitionedBy10By7 } = useLoaderData<typeof loader>();
-//   console.log(partitionedBy10By7) //3D array
+  const { partitionedBy10, partitionedBy10By7 } =
+    useLoaderData<typeof loader>();
 
-    const renderCardsInColumn = (roomsByBlock: any[]) =>{
-        const cardItems = []
-        for (let i = 0; i < roomsByBlock.length; i++){
-            cardItems.push(<div key={i}>
-                <Card
-                  className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-                >
-                  <CardContent>
-                    <form method="post">
-                      {/* <input type="hidden" value={[entry]} name="_action" /> */}
-                      <Button className="bg-red-500 rounded text-white">
-                        Reserve
-                      </Button>
-                    </form>
-                  </CardContent>
-                </Card>
-              </div>)
-        }
-        return cardItems
-    //     return <div key={index}>
-    //     <Card
-    //       key={index}
-    //       className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-    //     >
-    //       <CardContent>
-    //         {numAvailableRooms}
-    //         <form method="post">
-    //           <input type="hidden" value={[entry]} name="_action" />
-    //           <Button className="bg-red-500 rounded text-white">
-    //             Reserve
-    //           </Button>
-    //         </form>
-    //       </CardContent>
-    //     </Card>
-    //   </div>
+  const renderCardsInColumn = (roomsByBlock: any[], partitionIndex: number) => {
+    const cardItems = [];
+
+    for (let i = 0; i < roomsByBlock.length; i++) {
+      const availableRoomIdArr: object[] = [];
+      roomsByBlock[i].map((item: any) => {
+        availableRoomIdArr.push({ id: item.id, time: item.time });
+      });
+      cardItems.push(
+        <div key={i}>
+          <Card className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600">
+            <CardContent>
+              <form method="post">
+                <input
+                  type="hidden"
+                  name="items"
+                  value={JSON.stringify(roomsByBlock[i])}
+                />
+                <input type="hidden" name="time" value={i} />
+                <button className="bg-red-500 rounded text-white">
+                  Reserve
+                </button>
+              </form>
+            </CardContent>
+          </Card>
+        </div>,
+      );
     }
+    return cardItems;
+  };
 
-    // const renderSingleCardItem = (roomsByBlock: any[], ) =>{
-    //     return <div key={index}>
-    //                 <Card
-    //                   key={index}
-    //                   className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-    //                 >
-    //                   <CardContent>
-    //                     {numAvailableRooms}
-    //                     <form method="post">
-    //                       <input type="hidden" value={[entry]} name="_action" />
-    //                       <Button className="bg-red-500 rounded text-white">
-    //                         Reserve
-    //                       </Button>
-    //                     </form>
-    //                   </CardContent>
-    //                 </Card>
-    //               </div>
-        
-    // }
-
-    
   return (
     <div>
       <p>Reserve</p>
       <form method="post">
-        <input type="hidden" value="reserve" name="_action" />
-        <Button className="bg-red-500 rounded text-white">Reserve</Button>
+        <input type="hidden" name="title" value="title 1" />
+        <input type="hidden" name="title" value="title 2" />
+        <input type="hidden" name="title" value="title 3" />
+        <button className="bg-red-500 rounded text-white">Reserve</button>
       </form>
       <div className="flex h-full bg-white">
-        {partitionedBy10By7.map((item: any, index: any) => {
-          //this is one column of 7 cards, each card contains array of 10 rooms
-          //[[Array(10), Array(10),....]] => length = 7
-          
-          return (
-            <div className="h-full w-40 border-r bg-gray-50" key={index}>
-              {item.map((entry: any, index: any) => {
-                const sum = entry.reduce(
-                  (arr: any, curr: any) => arr + curr.time,
-                  0,
-                );
-                let numAvailableRooms = 0;
-                entry.map((item: any) =>
-                  item.booked_user_id === 0
-                    ? (numAvailableRooms += 1)
-                    : undefined,
-                );
-                const entryArr = [entry]
-                // console.log(`entryArr ${entryArr[0][9].id}`)
-                return (
-                  <div key={index}>
-                    <Card
-                      key={index}
-                      className="my-2 h-20 flex items-center justify-center rounded bg-yellow-500 px-4 py-3 font-medium text-white hover:bg-yellow-600"
-                    >
-                      <CardContent>
-                        {numAvailableRooms}
-                        <form method="post">
-                          <input type="hidden" value={[entry]} name="_action" />
-                          <Button className="bg-red-500 rounded text-white">
-                            Reserve
-                          </Button>
-                        </form>
-                      </CardContent>
-                    </Card>
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
         <div className="h-full w-40 border-r bg-gray-50" key={0}>
-            {renderCardsInColumn(partitionedBy10By7[0])}
+          {renderCardsInColumn(partitionedBy10By7[0], 0)}
         </div>
-        <div className="h-full w-40 border-r bg-gray-50" key={1}></div>
-        <div className="h-full w-40 border-r bg-gray-50" key={2}></div>
-        <div className="h-full w-40 border-r bg-gray-50" key={3}></div>
-        <div className="h-full w-40 border-r bg-gray-50" key={4}></div>
-        <div className="h-full w-40 border-r bg-gray-50" key={5}></div>
-        <div className="h-full w-40 border-r bg-gray-50" key={6}></div>
+        <div className="h-full w-40 border-r bg-gray-50" key={1}>
+          {renderCardsInColumn(partitionedBy10By7[1], 1)}
+        </div>
+        <div className="h-full w-40 border-r bg-gray-50" key={2}>
+          {renderCardsInColumn(partitionedBy10By7[2], 2)}
+        </div>
+        <div className="h-full w-40 border-r bg-gray-50" key={3}>
+          {renderCardsInColumn(partitionedBy10By7[3], 3)}
+        </div>
+        <div className="h-full w-40 border-r bg-gray-50" key={4}>
+          {renderCardsInColumn(partitionedBy10By7[4], 4)}
+        </div>
+        <div className="h-full w-40 border-r bg-gray-50" key={5}>
+          {renderCardsInColumn(partitionedBy10By7[5], 5)}
+        </div>
+        <div className="h-full w-40 border-r bg-gray-50" key={6}>
+          {renderCardsInColumn(partitionedBy10By7[6], 6)}
+        </div>
         <div className="flex-1 p-6">
           <Outlet />
         </div>
@@ -163,14 +101,3 @@ export default function DashboardReserve() {
     </div>
   );
 }
-
-//render each time block in a 7-day calendar
-//for-loop? Need a tag to indicate if time block is available
-//pseudo:
-/**
- * for each time block
- *      if available
- *          renders clickable button to reserve
- *      else
- *          renders non-clickable card
- */
