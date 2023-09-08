9e85bc62ddbc90eaa73167e4f99a1ac215bade90
Minh T. Nguyen <minhnguyen_2024@depauw.edu>
2023-09-08 14:51:54 -0400

    Update README

--- app/models/room.server.ts
+++ app/models/room.server.ts
@@ -1,24 +1,53 @@
 import { Prisma } from "@prisma/client";
 import { prisma } from "~/db.server";
 export type Room = {
-    accessible: number,
-    power: number,
-    reservable: number,
-    softSeating: number,
-    tableChairs: number,
-    monitor: number,
-    whiteboard: number,
-    window: number,
-}
-export async function createRoom({accessible, power, reservable, softSeating, tableChairs, monitor, whiteboard, window}: Room){
-    return await prisma.$executeRaw`INSERT INTO Room (accessible, power, reservable, softSeating, tableChairs, monitor, whiteboard, window) 
-    VALUES (${accessible}, ${power}, ${reservable}, ${softSeating}, ${tableChairs}, ${monitor}, ${whiteboard}, ${window})`
+    id?: number
+  accessible: number;
+  power: number;
+  reservable: number;
+  softSeating: number;
+  tableChairs: number;
+  monitor: number;
+  whiteboard: number;
+  window: number;
+};
+export async function createRoom({
+  accessible,
+  power,
+  reservable,
+  softSeating,
+  tableChairs,
+  monitor,
+  whiteboard,
+  window,
+}: Room) {
+  await prisma.$executeRaw`INSERT INTO Room (accessible, power, reservable, softSeating, tableChairs, monitor, whiteboard, window)
+    VALUES (${accessible}, ${power}, ${reservable}, ${softSeating}, ${tableChairs}, ${monitor}, ${whiteboard}, ${window})`;
+  const result: any[] = await prisma.$queryRaw`SELECT last_insert_rowid()`;
+  const newRoomId: number = Number(result[0]["last_insert_rowid()"]);
+  for (let i = 1; i < 50; i++) {
+    const block =
+      await prisma.$executeRaw`INSERT INTO Block (room_id, time, booked_user_id) VALUES (${newRoomId}, ${i}, ${0})`;
+  }
 }
 
-export async function deleteRoombyId({}){
-    return null
+//also delete block
+export async function deleteRoombyId({ roomId }: { roomId: number }) {
+    console.log("Deleting Room....")
+  const numDeletedRooms =
+    await prisma.$executeRaw`DELETE FROM Room where id = ${roomId}`;
+  const numDeletedBlocks =
+    await prisma.$executeRaw`DELETE FROM Block where id = ${roomId}`;
+    console.log(`Successful: ${numDeletedRooms} entry from Room table deleted`)
+    console.log(`Successful: ${numDeletedBlocks} entry from Block table deleted`)
+    
+  return null;
 }
 
 export async function updateRoom() {
-    return null
+  return null;
+}
+
+export async function selectAllRooms(): Promise<Room[]>{
+    return await prisma.$queryRaw`SELECT * From Room`
 }
--- app/routes/dashboard.admin.manageStudyRooms.delete.tsx
+++ app/routes/dashboard.admin.manageStudyRooms.delete.tsx
@@ -1,5 +1,76 @@
-export default function DeleteRoom(){
-    return <div>
-        
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { type LoaderArgs, type ActionArgs } from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { Room, selectAllRooms } from "~/models/room.server";
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const rooms: Room[] = await selectAllRooms();
+  return { rooms };
+};
+
+export const action = async ({ request }: ActionArgs) => {
+  const body = await request.formData();
+  const roomId = body.get("roomId");
+  console.log(roomId)
+  return null
+};
+
+//testing SQL delete model, will change to table with delete button, pop a modal
+export default function DeleteRoom() {
+  const { rooms } = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <Form method="post">
+        <input className="border" type="text" name="roomId" />
+        <label>Room ID:</label>
+      </Form>
+
+      <div>
+        <Table>
+          <TableCaption>A list of available Study Rooms</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead className="">Room #</TableHead>
+              <TableHead>Amenities</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rooms.map((item: any) => (
+              <TableRow className="border rounded" key={item.roomId}>
+                <TableCell>{item.roomId}</TableCell>
+                <TableCell>
+                  {item.accessible == 1 ? "Accessible, " : " "}
+                  {item.power == 1 ? "Power, " : " "}
+                  {item.reservable == 1 ? "Reservable, " : " "}
+                  {item.softSeating == 1 ? "Soft-seating, " : " "}
+                  {item.tableChairs == 1 ? "Table and Chairs, " : " "}
+                  {item.monitor == 1 ? "Monitor, " : " "}
+                  {item.whiteboard == 1 ? "Whiteboard, " : " "}
+                  {item.window == 1 ? "Window, " : " "}
+                </TableCell>
+                <TableCell>
+                  <form method="post">
+                    <input type="hidden" value={item.id} name="roomId" />
+                    <Button className="border rounded bg-green-500 text-white">
+                      Delete
+                    </Button>
+                  </form>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
     </div>
+  );
 }
