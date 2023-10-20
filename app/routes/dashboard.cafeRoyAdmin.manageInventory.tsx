383aeebc70619bffb9af004bfa438e16f4ef6184
Minh T. Nguyen <minhnguyen_2024@depauw.edu>
2023-10-20 15:36:06 -0400

    add prisma db push

--- app/routes/dashboard.cafeRoyAdmin.manageInventory.tsx
+++ app/routes/dashboard.cafeRoyAdmin.manageInventory.tsx
@@ -1,7 +1,4 @@
-import { type LoaderArgs} from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
-import { getAllUsers, getUserById } from "~/models/user.server";
-import { requireUserId } from "~/session.server";
 
 export default function DashboardCafeRoyAdminManageInventory() {
   return (
