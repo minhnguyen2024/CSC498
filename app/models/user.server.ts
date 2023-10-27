ae00824cc4c3e37b4b6fb2e4a3fcafd4b9deb361
Minh T. Nguyen <minhnguyen_2024@depauw.edu>
2023-10-27 09:53:02 -0400

    cy

--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -16,7 +16,7 @@ export async function getUserByUsername(username: string) {
 
 export async function verifyLogin(username: string, password: string): Promise<User> {
   const existingUser: User[] =
-    await prisma.$queryRaw`SELECT * FROM User`;
+    await prisma.$queryRaw`SELECT * FROM User WHERE username = ${username}`;
   const userWithPassword = await prisma.user.findUnique({
     where: { username },
   });
